b24c8dad2aee07aa1aaa5ff1ce968867ffbec9d9
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-07-31 22:24:50 -0400

    test

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const server = require('./api/server.js');
+require('dotenv').config()
+const port = process.env.PORT ? process.env.PORT : 4000
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+//not required for PORT to be in capitals but it is recommended
+//for env vars to be in capitals
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
